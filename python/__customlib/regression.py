fe2e933bf0fdf8231cf20fd443b53282b7be1c89
Faris <madebyais@gmail.com>
2017-10-29 19:33:59 +0700

    Removed unused variable.

--- python/__customlib/regression.py
+++ python/__customlib/regression.py
@@ -4,6 +4,7 @@ class Regression:
 
     def calculate(self, values):
         regr = linear_model.LinearRegression()
+
         # split the values into two series instead a list of tuples
         x, y = zip(*values)
         max_x = max(x)
@@ -12,10 +13,10 @@ class Regression:
         # split the values in train and data.
         train_data_X = map(lambda x: [x], list(x))
         train_data_Y = list(y)
-        test_data_X = map(lambda x: [x], list(x))
-        test_data_Y = list(y)
+
         # feed the linear regression with the train data to obtain a model.
         regr.fit(train_data_X, train_data_Y)
+
         # check that the coeffients are the expected ones.
         m = regr.coef_[0]
         b = regr.intercept_
